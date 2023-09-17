e33cf848c50b33bf7f75e3974e4efb8a14205800
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-09-17 02:55:06 -0500

    Update grid.gd
    pieces spawn on board randomly

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -7,12 +7,22 @@ extends Node2D
 @export var y_start: int;
 @export var offset: int;
 
+var possible_pieces = [
+preload("res://Scenes/yellow_piece.tscn"),
+preload("res://Scenes/blue_piece.tscn"),
+preload("res://Scenes/green_piece.tscn"),
+preload("res://Scenes/light_green_piece.tscn"),
+preload("res://Scenes/orange_piece.tscn"),
+preload("res://Scenes/pink_piece.tscn")
+];
+
 var all_pieces = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize();
 	all_pieces = make_2d_array();
-	print(all_pieces);
+	spawn_pieces();
 
 func make_2d_array():
 	var array = [];
@@ -21,6 +31,19 @@ func make_2d_array():
 		for j in height:
 			array[i].append(null);
 	return array;
+	
+func grid_to_pixel(column, row):
+	var new_x = x_start + offset * column;
+	var new_y = y_start + -offset * row;
+	return Vector2(new_x, new_y);
+	
+func spawn_pieces():
+	for i in width:
+		for j in height:
+			var rand = randi_range(0, possible_pieces.size()-1);
+			var piece = possible_pieces[rand].instantiate();
+			add_child(piece);
+			piece.set_position(grid_to_pixel(i, j));
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
