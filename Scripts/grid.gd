3a2d171c74dc26a3dd9430ace64b1d8fd8440ada
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-09-17 03:41:30 -0500

    No Matches
    No Matches when tiles spawn

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -31,19 +31,42 @@ func make_2d_array():
 		for j in height:
 			array[i].append(null);
 	return array;
+
+func spawn_pieces():
+	for i in width:
+		for j in height:
+			# choose random number and store it
+			var rand = randi_range(0, possible_pieces.size()-1);
+			# instantiate that piece from the array
+			var piece = possible_pieces[rand].instantiate();
+			var loops = 0;
+			while(match_at(i, j, piece.color) && loops < 100):
+				rand = randi_range(0, possible_pieces.size()-1);
+				loops += 1;
+				piece = possible_pieces[rand].instantiate();
+			add_child(piece);
+			# place piece on board
+			piece.set_position(grid_to_pixel(i, j));
+			all_pieces[i][j] = piece;
+
+func match_at(i, j, color):
+	if i > 1:
+		if all_pieces[i - 1][j] != null && all_pieces[i - 2][j] != null:
+			if all_pieces[i - 1][j].color == color && all_pieces[i - 2][j].color == color:
+				return true;
+	if j > 1:
+		if all_pieces[i][j - 1] != null && all_pieces[i][j - 2] != null:
+			if all_pieces[i][j - 1].color == color && all_pieces[i][j - 2].color == color:
+				return true;
+	pass;
 	
 func grid_to_pixel(column, row):
 	var new_x = x_start + offset * column;
 	var new_y = y_start + -offset * row;
 	return Vector2(new_x, new_y);
 	
-func spawn_pieces():
-	for i in width:
-		for j in height:
-			var rand = randi_range(0, possible_pieces.size()-1);
-			var piece = possible_pieces[rand].instantiate();
-			add_child(piece);
-			piece.set_position(grid_to_pixel(i, j));
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
